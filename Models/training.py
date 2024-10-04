dd8a9c86b91293d96022ccbd5e5a70f26578cba1
Gargee PHUKON <gargee161011@gmail.com>
2024-10-04 17:50:28 +0200

    Update: Code refactoring, early stopping and moving average

--- Models/training.py
+++ Models/training.py
@@ -1,28 +1,26 @@
 import numpy as np
 import os
-import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader, ConcatDataset
-from torchvision import transforms
-from dataloader_2d_segmentation import SegmentationDataset
-from CustomUnetWithSkip import CustomUnetWithSkip
-from CustomUnet import CustomUnet
-import Utils
-# from unet_pytorch_segmentation import UNet
 import torch.nn.functional as F
+from torch.utils.data import DataLoader, Subset
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score
-import tqdm 
-from dataloader_weka import ImageMaskDataset 
 from torch.utils.tensorboard import SummaryWriter
+from tqdm import tqdm
+
+from segmentation_dataset import SegmentationDataset 
+from custom_unet_with_skip import CustomUnetWithSkip
+from image_mask_dataset import ImageMaskDataset 
+from early_stopping import EarlyStopping
+import Utils  # Assuming Utils contains relevant utility functions
 
 
 # Tune Hyperparameters (learning rate, epoch, batch size, seed, optimizer)
-learning_rate=0.001
+learning_rate=0.01
 num_epochs = 10000
-batch_size= 16
+batch_size= 12
 random_seed=42
 optimizer ="SGD" 
 do_augmentation=True
@@ -31,7 +29,7 @@ activation = "ReLU"
 # Initialize SummaryWriter
 unet_depth= "4"
 naming=optimizer
-exp_name=naming+"_bs_"+str(batch_size)+"__lr_"+str(learning_rate)+"__epoc_"+str(num_epochs)+"__optim_"+str(optimizer)+"__unet_depth_"+str(unet_depth)+"__augmentation_"+str(do_augmentation)+"__activation_"+str(activation)+"__"
+exp_name=naming+"_bs_"+str(batch_size)+"__lr_"+str(learning_rate)+"__epoc_"+str(num_epochs)+"__optim_"+str(optimizer)+"__unet_depth_"+str(unet_depth)+"__augmentation_"+str(do_augmentation)+"__activation_"+str(activation)+"__EWMA_val_loss_"
 log_dir='/home/phukon/Desktop/Model_Fitting/runs/training_custom_unet_with_skip_connections_'+exp_name
 
 # Create directory
@@ -68,6 +66,9 @@ optimizer = optim.SGD(model.parameters(), lr=learning_rate)
 best_val_loss = float('inf')  # Initialize with infinity
 best_epoch = -1  # Track the epoch with minimum validation loss
 best_model_path = os.path.join(model_dir, exp_name+"best_model.pth")  # Path to save the best model
+window_size = 20  
+moving_avg_val_loss = 0
+early_stopping = EarlyStopping(patience=100, min_delta=0.01)
 
 # Train and validate
 for epoch in range(num_epochs):
@@ -108,7 +109,7 @@ for epoch in range(num_epochs):
     writer.add_scalar('F1/train', train_f1, epoch)
     writer.add_scalar('F1/train/hf', train_f1_hf, epoch)
     writer.add_scalar('F1/train/necd', train_f1_necd, epoch)
-    writer.add_scalar('Loss/train', running_loss, epoch)
+    writer.add_scalar('Loss/train', running_loss/ len(train_loader), epoch)
 
 
     # Validation loop
@@ -130,27 +131,48 @@ for epoch in range(num_epochs):
             all_labels = masks.flatten().cpu().numpy()  # Ensure the labels are flattened and moved to CPU
             all_preds = torch.argmax(y_pred, dim=1).flatten().cpu().numpy()  # Get predicted class indices and flatten
             # all_preds = torch.argmax(y_pred[:,0:7,:,:], dim=1).flatten().cpu().numpy()  # Get predicted class indices and flatten
+    
+    val_loss = val_loss / len(val_loader)
+    if epoch == 0:
+        moving_avg_val_loss = val_loss  # Initialize the moving average with the first value
+    else:
+        moving_avg_val_loss = ((window_size - 1) / window_size) * moving_avg_val_loss + (1 / window_size) * val_loss
 
+    
     # Calculate validation F1 score
     val_f1 = f1_score(all_labels, all_preds, labels=[0,1,2,3,4,5,6],average='weighted') #
-    val_f1_hf=f1_score(all_labels, all_preds, labels=[0,1],average='weighted')
+    val_f1_hf=f1_score(all_labels, all_preds, labels=[0],average='weighted')
     val_f1_necd=f1_score(all_labels, all_preds, labels=[2,3],average='weighted')
     writer.add_scalar('F1/val', val_f1, epoch)
     writer.add_scalar('F1/val/hf', val_f1_hf, epoch)
     writer.add_scalar('F1/val/necd', val_f1_necd, epoch)
-    writer.add_scalar('Loss/val', val_loss, epoch)
+    writer.add_scalar('Loss/val', moving_avg_val_loss, epoch)
 
-    print(f"[{epoch + 1}/{num_epochs}], TRLoss: {running_loss / len(train_loader):.4f}, ValLoss: {val_loss / len(val_loader):.4f}, TrainF1: {train_f1:.4f}, ValF1: {val_f1:.4f}, ValF1hf: {val_f1_hf:.4f}, ValF1necd: {val_f1_necd:.4f}")
+    print(f"[{epoch + 1}/{num_epochs}], TRLoss: {running_loss / len(train_loader):.4f}, ValLoss: {val_loss:.4f}, TrainF1: {train_f1:.4f}, ValF1: {val_f1:.4f}, ValF1hf: {val_f1_hf:.4f}, ValF1necd: {val_f1_necd:.4f}")
+
+    #Early Stopping check
+    early_stopping.check(moving_avg_val_loss) 
+    if early_stopping.should_stop:
+        print(f"Early stopping at epoch {epoch}. Best validation loss: {early_stopping.best_val_loss:.4f}")
+        break
+
+    if moving_avg_val_loss < best_val_loss:
+            best_val_loss = moving_avg_val_loss  # Update the lowest validation moving average loss
+            corresponding_train_loss = running_loss 
+            best_epoch = epoch  # Update the best epoch
+            torch.save(model.state_dict(), best_model_path)  # Save the model's state dict
+            print(f"New best model saved at epoch {epoch + 1} with train loss {running_loss / len(train_loader):.4f}, val_loss {val_loss:.4f}, moving avg val_loss: {moving_avg_val_loss:.4f}")
+
     
-    if val_loss < best_val_loss:
-        best_val_loss = val_loss  # Update the lowest validation loss
-        corresponding_train_loss = running_loss 
-        best_epoch = epoch  # Update the best epoch
-        torch.save(model.state_dict(), best_model_path)  # Save the model's state dict
-        print(f"New best model saved at epoch {epoch + 1} with train loss {running_loss/ len(train_loader):.4f} and val_loss {val_loss:.4f}")
+    # if val_loss < best_val_loss:
+    #     best_val_loss = val_loss  # Update the lowest validation loss
+    #     corresponding_train_loss = running_loss 
+    #     best_epoch = epoch  # Update the best epoch
+    #     torch.save(model.state_dict(), best_model_path)  # Save the model's state dict
+    #     print(f"New best model saved at epoch {epoch + 1} with train loss {running_loss/ len(train_loader):.4f} and val_loss {val_loss:.4f}")
 
 
-print(f"Training complete. Best validation loss {best_val_loss:.4f} and train loss {running_loss/ len(train_loader):.4f} at epoch {best_epoch + 1}")
+print(f"Training complete. Best validation loss {best_val_loss:.4f} at epoch {best_epoch + 1}")
     
 
 writer.close()
